bc19cac1b802cde101bf85f27bb394a12972e4fa
Jkmo72 <47998782+Jkmo72@users.noreply.github.com>
2019-04-17 08:36:03 -0500

    Update Topologia.py

--- Topologia.py
+++ Topologia.py
@@ -1,6 +1,6 @@
-from mininet.topo import Topo
+
 class MyTopo( Topo ):
-    "Simple topology example." 
+    "Simple topology example."
   def __init__( self ):
         "Create custom topo."
         # Initialize topology
@@ -10,13 +10,13 @@ class MyTopo( Topo ):
         Host2 = self.addHost( 'h2' )
         Host3 = self.addHost( 'h3' )
         Host4 = self.addHost( 'h4' )
-	ServerWeb = self.addHost( 'SrvWeb' )	
+        ServerWeb = self.addHost( 'SrvWeb' )
         Switch1 = self.addSwitch( 's1' )
         Switch2 = self.addSwitch( 's2' )
         # Add links
         self.addLink( Host1, Switch1 )
-        self.addLink( Host2, Switch1 )	
-	self.addLink( Host3, Switch1 )
+        self.addLink( Host2, Switch1 )
+        self.addLink( Host3, Switch1 )
         self.addLink( Switch1, Switch2 )
        self.addLink( Switch2, Host4 )
         self.addLink( Switch2, ServerWeb )
