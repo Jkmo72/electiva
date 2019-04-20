feb4331febce5bc07416fd786176661a1420959e
Jkmo72 <47998782+Jkmo72@users.noreply.github.com>
2019-04-20 14:30:39 -0500

    Update l2_learning.py

--- l2_learning.py
+++ l2_learning.py
@@ -110,11 +110,10 @@ class LearningSwitch (object):
     """
 
     packet = event.parsed
-    if packet.type == packet.ICMP_TYPE:
+    if packet.type == packet.IP_TYPE:
 	ip_packet = packet.payload
-	#ip_origen = ip_packet.srcip
-	print "El Payload es: ", ip_packet
-	#print "La MAC de origen es:  ", packet.src 
+	print "El Payload del paquete es: ", ip_packet
+	
 
 
     def flood (message = None):
