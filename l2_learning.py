3472f9730ca098e7e748b0143ae11a5e685a8842
Jkmo72 <47998782+Jkmo72@users.noreply.github.com>
2019-04-20 13:57:02 -0500

    Update l2_learning.py

--- l2_learning.py
+++ l2_learning.py
@@ -1,5 +1,4 @@
 # Copyright 2011-2012 James McCauley
-# Hola estos esuna pruba
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at:
@@ -11,6 +10,10 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+# Codigo proyecto electica 4 Camilo Bohada - Camilo Jerez
+# Unitec
+
 
 """
 An L2 learning switch.
