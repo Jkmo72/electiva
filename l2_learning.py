8c710492e73ffac20568951c7c6864ee95016b53
Jkmo72 <47998782+Jkmo72@users.noreply.github.com>
2019-05-26 12:57:13 -0500

    Update l2_learning.py

--- l2_learning.py
+++ l2_learning.py
@@ -120,19 +120,19 @@ class LearningSwitch (object):
     if tcp is not None and tcp.parsed:
 		print "Camilo Jerez & Camilo Bohada"
 		print tcp.payload.decode("utf-8")
-		#if (tcp.payload.find('www') == 0): 
-   		#if tcp.find('www') == word.find('www'):
-		# print ("Conectado")
-		 			
-		#else: 
-		 # print ("Acceso Denegado")
-		  #bloquear = 1
-			
-		if tcp.find('www') == word.find('www'):
-		  print ("Acceso Denegado")
+		 if (tcp.payload.find('www') == 0): 
+   		 if tcp.find('www') == word.find('www'):
+		 print ("Conectado")
 		 			
 		else: 
-		  print ("Conectado")
+		  print ("Acceso Denegado")
+		  bloquear = 1
+			
+		#if tcp.find('www') == word.find('www'):
+		  #print ("Acceso Denegado")
+		 			
+		#else: 
+		  #print ("Conectado")
 		  	
 
     #------------------------------------------------------------------------|
