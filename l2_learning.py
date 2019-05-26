6bc7c49dd3d06f9a79dc0f706a4d78dcc89eca36
Jkmo72 <47998782+Jkmo72@users.noreply.github.com>
2019-05-26 13:11:10 -0500

    Update l2_learning.py

--- l2_learning.py
+++ l2_learning.py
@@ -1,5 +1,5 @@
 # Copyright 2011-2012 James McCauley
-# Hola estos esuna pruba
+# Camilo Jerez & Camilo Bohada
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at:
